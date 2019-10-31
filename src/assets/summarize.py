e8107ff30e97f79f71a0b7ec074526987d0ffcb3
zvonimirpetrovic <zvonimir.petrovic10@gmail.com>
2019-10-31 11:51:29 +0100

    Created a server using nodejs, connected summarizing python script to server

---
+++ src/assets/summarize.py
@@ -0,0 +1,177 @@
+
+# coding=UTF-8
+import re
+import sys
+import json
+
+alphabets = "([A-Ža-ž])"
+capitals = "([A-Ž])"
+lowercase = "([a-ž])"
+prefixes = "(g|gosp|gđa|gđica|dr|mag|sc)[.]"
+suffixes = "(d|)[.]"
+starters = "(Gosp|Gđa|Gđica|Dr|On|Ona|Ono|Oni|One|Ona|Mi|Vi|Ali|Nego|Već|Iako|Tako|Ovo|Štogod|Koliko|Kao|Kako|Danas|Jučer|Tada)"
+acronyms = "([A-Ž][.][A-Ž][.](?:[A-Ž][.])?)"
+months = "[.](siječnja|siječanj|veljače|veljača|ožujka|ožujak|travnja|travanj|svibnja|svibanj|lipnja|lipanj|srpnja|srpanj|kolovoza|kolovoz|rujna|rujan|listopada|listopad|studenog|studeni|prosinca|prosinac)"
+digits = "([0-9])"
+dates = "([0-9])[.]"
+
+class SummaryTool(object):
+
+    # Naive method for splitting a text into sentences
+    def split_content_to_sentences(self, content):
+        content = " " + content + "  "
+        content = content.replace("\n"," ")
+        content = re.sub(prefixes, "\\1<prd>", content)
+        content = re.sub(months, "<prd>\\1", content)
+        if "Ph.D" in content: content = content.replace("Ph.D.","Ph<prd>D<prd>")
+        content = re.sub("\s" + alphabets + "[.] "," \\1<prd> ", content)
+        content = re.sub(acronyms+" "+starters,"\\1<stop> \\2", content)
+        content = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>", content)
+        content = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>", content)
+        content = re.sub(" " + suffixes + "[.] " + starters," \\1<stop> \\2", content)
+        content = re.sub(" " + suffixes + "[.]"," \\1<prd>", content)
+        content = re.sub(" " + alphabets + "[.]"," \\1<prd>", content)
+        content = re.sub(dates + " " + lowercase,"\\1<prd> \\2", content)
+        content = re.sub(digits + "[.]" + digits, "\\1<prd>\\2", content)
+
+        if "”" in content:
+            content = content.replace(".”","”.")
+        if "\"" in content:
+            content = content.replace(".\"","\".")
+        if "!" in content:
+            content = content.replace("!\"","\"!")
+        if "?" in content:
+            content = content.replace("?\"","\"?")
+        content = content.replace(".",".<stop>")
+        content = content.replace("?","?<stop>")
+        content = content.replace("!","!<stop>")
+        content = content.replace("<prd>",".")
+        sentences = content.split("<stop>")
+        sentences = sentences[:-1]
+        content = [s.strip() for s in sentences]
+        return content
+        #content = content.replace("\n", ". ")
+        #return content.split(". ")
+
+    # Naive method for splitting a text into paragraphs
+    def split_content_to_paragraphs(self, content):
+        return content.split("\n\n")
+
+    # Caculate the intersection between 2 sentences
+    def sentences_intersection(self, sent1, sent2):
+
+        # split the sentence into words/tokens
+        s1 = set(sent1.split(" "))
+        s2 = set(sent2.split(" "))
+
+        # If there is not intersection, just return 0
+        if (len(s1) + len(s2)) == 0:
+            return 0
+
+        # We normalize the result by the average number of words
+        return len(s1.intersection(s2)) / ((len(s1) + len(s2)) / 2)
+
+    # Format a sentence - remove all non-alphbetic chars from the sentence
+    # We'll use the formatted sentence as a key in our sentences dictionary
+    def format_sentence(self, sentence):
+        sentence = re.sub(r'\W+', '', sentence)
+        return sentence
+
+    # Convert the content into a dictionary <K, V>
+    # k = The formatted sentence
+    # V = The rank of the sentence
+    def get_sentences_ranks(self, content):
+
+        # Split the content into sentences
+        sentences = self.split_content_to_sentences(content)
+
+        # Calculate the intersection of every two sentences
+        n = len(sentences)
+        values = [[0 for x in range(n)] for x in range(n)]
+        for i in range(0, n):
+            for j in range(0, n):
+                values[i][j] = self.sentences_intersection(sentences[i], sentences[j])
+
+        # Build the sentences dictionary
+        # The score of a sentences is the sum of all its intersection
+        sentences_dic = {}
+        for i in range(0, n):
+            score = 0
+            for j in range(0, n):
+                if i == j:
+                    continue
+                score += values[i][j]
+            sentences_dic[self.format_sentence(sentences[i])] = score
+        return sentences_dic
+
+    # Return the best sentence in a paragraph
+    def get_best_sentence(self, paragraph, sentences_dic):
+
+        # Split the paragraph into sentences
+        sentences = self.split_content_to_sentences(paragraph)
+
+
+        # Ignore short paragraphs
+        if len(sentences) < 2:
+            return ""
+
+        # Get the best sentence according to the sentences dictionary
+        best_sentence = ""
+        max_value = 0
+        for s in sentences:
+            strip_s = self.format_sentence(s)
+            if strip_s:
+                if sentences_dic[strip_s] > max_value:
+                    max_value = sentences_dic[strip_s]
+                    best_sentence = s
+
+        return best_sentence
+
+    # Build the summary
+    def get_summary(self, title, content, sentences_dic):
+
+
+        # Split the content into paragraphs
+        paragraphs = self.split_content_to_paragraphs(content)
+
+        # Add the title
+        summary = []
+        summary.append(title.strip())
+        summary.append("")
+
+        # Add the best sentence from each paragraph
+        for p in paragraphs:
+            sentence = self.get_best_sentence(p, sentences_dic).strip()
+            if sentence:
+                summary.append(sentence)
+
+        return ("\n").join(summary)
+
+def main():
+
+    title = sys.argv[1]
+
+    content = sys.argv[2]
+
+    # Create a SummaryTool object
+    st = SummaryTool()
+
+    # Build the sentences dictionary
+    sentences_dic = st.get_sentences_ranks(content)
+
+    # Build the summary with the sentences dictionary
+    summary = st.get_summary(title, content, sentences_dic)
+
+    # Print the summary
+    print (summary)
+
+    # Print the ratio between the summary length and the original length
+    print ("")
+    print ("Original Length %s" % (len(title) + len(content)))
+    print ("Summary Length %s" % len(summary))
+    print ("Summary Ratio: %s" % (100 - (100 * (len(summary) / (len(title) + len(content))))))
+    print (my_list)
+    sys.stdout.flush()
+
+if __name__ == '__main__':
+    main()
---
+++ src/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.static(__dirname + '/app/'))
+
+app.get('*.*', (req,res) => res.sendFile(path.join(__dirname)));
+
+
+let title = '"""""""';
+
+let content = `""
+"AMERIČKI predsjednik Donald Trump nahvalio je svog mađarskog kolegu Viktora Orbana pri susretu u Bijeloj kući. Među ostalim, rekao je kako mu je "velika čast" primiti ga u posjet, kako je Orban napravio "ogroman posao na toliko različitih načina" i kako je "vrlo poštovan diljem Europe" - što evidentno nije istina.
+
+"Vjerojatno je, kao i ja, pomalo kontroverzan, ali to je ok", najbliže je kritici - iako to ustvari nije - što je Trump došao prilikom susreta s mađarskim ekstremno desnim populistom. "Učinili ste dobar posao i održali svoju zemlju sigurnom", odmah je dodao.
+
+To i nije iznenađujuće jer Trump i Orban imaju puno više zajedničkog nego onoga što ih dijeli. Prvenstveno je to - "borba protiv ilegalne imigracije", kao što je sam Orban naglasio.
+
+"Učinili ste dobru stvar u vezi imigracije, mnogi to misle", uvjeravao ga je Trump, hvaleći ga da je zaista "podigao blok", u aluziji na mađarsko postavljanje žičane ograde na granici sa Srbijom i Hrvatskom prije nekoliko godina. Ograde zbog koje je, podsjetimo, migrantski val preusmjeren prvo preko Hrvatske, a sada preko BiH pa onda preko Hrvatske. 
+
+U priopćenju Bijele kuće nakon susreta navodi se kako su "dvojica lidera potvrdila svoju predanost NATO savezu i demokratskom sustavu vladavine koji osigurava slobodu i kultivira prosperitet koji SAD i Mađarska uživaju". No kako brojni kritičari upozoravaju, upravo su Trump i Orban ti koji nasrću na demokratski sustav vladavine i podrivaju ga iznutra. O NATO-u, koji Trump shvaća kao zaštitni reket koji druge zemlje trebaju plaćati SAD-u, da ne govorimo.
+
+
+
+Trumpovi kritičari su, kako piše Guardian, opisali Orbanov posjet Bijeloj kući (na Trumpov poziv, naravno) kao posljednju potvrdu Trumpovog preferiranja suradnje sa stranim liderima "čvrste ruke", bez obzira na njihovo gušenje ljudskih prava i građanskih sloboda.
+
+"Orban predstavlja toliko stvari koje su antiteza središnjim američkim vrijednostima. On je nadgledao unazađivanje demokracije u svojoj zemlji, koristio antisemitske i ksenofobne izraze u svojoj političkoj komunikaciji i zbližio se s Vladimirom Putinom i Kremljem. Zabrinjava nas što vidimo da se predsjednik naše zemlje, koja je kroz povijest podržavala zaštitu i promicanje demokracije i ljudskih prava diljem svijeta, susreće s čovjekom koji tako redovito zanemaruje ove vrijednosti", stoji u otvorenom pismu demokratskih kongresnika predvođenih Eliotom Engelom upućenom Trumpu prije samog posjeta, u kojem su tražili da se Orbanov posjet otkaže.
+
+Mađarska direktno prkosi SAD-u, Trump se dodvorava Orbanu
+
+Osim što je Orban u Mađarskoj nametnuo svoj model "iliberalne demokracije", kako ga je sam nazvao, temeljito potkopavajući neovisnost pravosuđa, medija, civilnog društva i ekonomije, Orban je, kako ističe direktorica Europskog programa u Centru za strateške i međunarodne studije Heather Conley, u više navrata išao protiv američkih interesa u regiji.
+
+Conley nabraja kako je Orban to učinio: kultivirajući bliske odnose s Putinom (iako je to, moglo bi se reći, učinio i sam Trump), pojačavajući svoju energetsku ovisnost o Rusiji, bilo putem novog plinovoda TurkStreama ili nuklearne elektrane Paks koju grade Rusi, ne suzbijajući kineski ekonomski utjecaj u Mađarskoj i Europi, dopuštajući Ruskoj međunarodnoj investicijskoj banci, koju se naširoko smatra instrumentom ruskih obavještajnih službi, da uspostavi sjedište u Budimpešti, odbijajući izručiti dvojicu osumnjičenih ruskih dilera oružja, blokirajući ukrajinske razgovore s NATO-om zbog zakona o službenom jeziku u toj zemlji i dajući azil odbjeglom bivšem premijeru Sjeverne Makedonije Nikoli Gruevskom protiv kojeg je u njegovoj zemlji podignuta optužnica zbog korupcije. 
+
+Svemu ovome može se pridodati i Orbanovu vulgarnu i dezinformirajuće propagandnu kampanju protiv Bruxellesa, predsjednika Europske komisije Jean-Claude Junckera, židovsko-mađarskog milijardera i aktivista Georgea Sorosa i njegovog Srednjeeuropskog sveučlišta, zbog čega je i Europska pučka stranka suspendirala članstvo Orbanovog Fidesza.
+
+"Ovo je ponižavajuće za SAD."
+
+"Nagrađujete ovu opstrukciju čašću sastanka u Ovalnom uredu (Bijele kuće)?" napisala je Conley u tekstu naslovljenom "Umijeće iliberalnog dogovora" na stranici svog centra i dodala: "Ovo je ponižavajuće za SAD."
+
+Štoviše, iako je Trump već mnogo puta grmio protiv NATO saveznica koje ne "plaćaju dovoljno SAD-u" - odnosno, prevedeno na jezik činjenica, ne ulažu dovoljno u svoj nacionalni vojni proračun koji posljedično jača zajedničku obranu saveza - Orbana nije prekorio za isto, iako Mađarska ulaže samo 1,15% svog BDP-a u obranu. To je vrlo daleko od dogovorene smjernice za NATO članice od 2% BDP-a. 
+
+Znakovito je da je posljednji put kad je Orban posjetio Bijelu kuću bio davne 1998., kad je predsjednik bio Bill Clinton. Ni George W. Bush ni Barack Obama nisu ga pozvali za vrijeme svog mandata. Nedavni posjet Mikea Pompea Budimpešti bio je prvi posjet američkog državnog tajnika od 2011.
+
+No umjesto da nametne stroge i konkretne uvjete za ponovno zatopljavanje odnosa - udaljavanje od Rusije i Kine, suzbijanje korupcije, prestanak represije nad nevladinim udrugama, prestanak sabotiranja ukrajinskih pregovora s NATO-om - SAD tolerira jogunjenje nelojalnog saveznika zbog Trumpovih simpatija prema autoritarno nastrojenim populistima. 
+
+
+"""`;
+
+
+//Python
+let spawn = require("child_process").spawn;
+let pythonProcess = spawn('python',["./assets/summarize.py", title, content]);
+
+pythonProcess.stdout.on('data', (data) => {
+         console.log(data.toString())
+});
+
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log('Server started')
+})
